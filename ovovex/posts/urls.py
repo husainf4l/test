6b38568a19a108c4642f562e32df2bff7cd29cc6
husain <husain.f4l@gmail.com>
2025-09-30 17:04:51 +0300

    Initial commit

---
+++ ovovex/posts/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.posts_list, name='list'),
+    path('<slug:slug>/', views.post_page, name='page'),
+
+]
+
